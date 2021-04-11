8b6f213665d45eca94e41adf36b49d2ae36d1d82
Victor Sarda <victor.sarda@icloud.com>
2021-04-11 20:56:34 +0100

    ✨ Added required extensions
    (fixed shadow)

--- ViteMaDose/Helpers/Extensions/UIImage+Commons.swift
+++ ViteMaDose/Helpers/Extensions/UIImage+Commons.swift
@@ -5,4 +5,20 @@
 //  Created by Victor Sarda on 11/04/2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    func tint(with fillColor: UIColor) -> UIImage? {
+        let image = withRenderingMode(.alwaysTemplate)
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        fillColor.set()
+        image.draw(in: CGRect(origin: .zero, size: size))
+
+        guard let imageColored = UIGraphicsGetImageFromCurrentImageContext() else {
+            return nil
+        }
+
+        UIGraphicsEndImageContext()
+        return imageColored
+    }
+}
