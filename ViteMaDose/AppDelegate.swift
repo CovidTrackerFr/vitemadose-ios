1ebf02c26da1183df5696bef1c1f2e3208e7c1a8
Paul Jeannot <pjeannot@semeia.io>
2021-04-14 01:38:16 +0200

    🔨 Init Firebase app

--- ViteMaDose/AppDelegate.swift
+++ ViteMaDose/AppDelegate.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+        FirebaseApp.configure()
 		return true
 	}
 
@@ -30,7 +29,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
 		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 	}
-
-
 }
-
