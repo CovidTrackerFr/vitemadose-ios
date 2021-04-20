d2fb519dce1a006f31fac4ab7e993bb6920d15e8
Nathan Fallet <contact@nathanfallet.me>
2021-04-20 17:11:04 +0200

    🎨 Updating header view with a description label

--- ViteMaDose/Views/Credits/Header/CreditHeaderView.swift
+++ ViteMaDose/Views/Credits/Header/CreditHeaderView.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class CreditHeaderView: UIView {
     @IBOutlet var titleLabel: UILabel!
-
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     private enum Constant {
-        static let titleText = "Notre magnifique équipe"
-        static let highlightedText = "équipe"
-        static let highlightedTextColor = UIColor.mandy
+        static let titleText = "Contributeurs"
+        static let descriptionText = "ViteMaDose est un projet open source, construit par l'ensemble de ses généreux contributeurs :"
+        static let highlightedText1 = "open source"
+        static let highlightedText2 = "généreux contributeurs"
+        static let highlightedTextColor1 = UIColor.royalBlue
+        static let highlightedTextColor2 = UIColor.mandy
         static let titleFont = UIFont.rounded(ofSize: 24, weight: .bold)
+        static let descriptionFont = UIFont.rounded(ofSize: 18, weight: .regular)
         static let searchBarFont = UIFont.rounded(ofSize: 16, weight: .medium)
         static let searchBarViewCornerRadius: CGFloat = 10.0
     }
@@ -23,6 +28,7 @@ class CreditHeaderView: UIView {
         super.awakeFromNib()
         backgroundColor = .athensGray
         configureTitle()
+        configureDescription()
     }
 
     private func configureTitle() {
@@ -33,10 +39,25 @@ class CreditHeaderView: UIView {
                 NSAttributedString.Key.foregroundColor: UIColor.label,
             ]
         )
-        attributedText.setColorForText(
-            textForAttribute: Constant.highlightedText,
-            withColor: Constant.highlightedTextColor
-        )
         titleLabel.attributedText = attributedText
+    }
+    
+    private func configureDescription() {
+        let attributedText = NSMutableAttributedString(
+            string: Constant.descriptionText,
+            attributes: [
+                NSAttributedString.Key.font: Constant.descriptionFont,
+                NSAttributedString.Key.foregroundColor: UIColor.label,
+            ]
+        )
+        attributedText.setColorForText(
+            textForAttribute: Constant.highlightedText1,
+            withColor: Constant.highlightedTextColor1
+        )
+        attributedText.setColorForText(
+            textForAttribute: Constant.highlightedText2,
+            withColor: Constant.highlightedTextColor2
+        )
+        descriptionLabel.attributedText = attributedText
     }
 }
