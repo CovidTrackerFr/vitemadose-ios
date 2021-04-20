2eb479c9968e478d6451ee1c36cc1f926110d398
Nathan Fallet <contact@nathanfallet.me>
2021-04-20 16:16:42 +0200

    🗑 Removing hardcoded store

--- ViteMaDose/Models/Credit.swift
+++ ViteMaDose/Models/Credit.swift
@@ -25,23 +25,3 @@ struct CreditSection: Codable {
 // MARK: - Credits
 
 typealias Credits = [CreditSection]
-
-// MARK: - Credit Store (TEMP)
-
-struct CreditStore {
-    static let store: Credits = [
-        CreditSection(section: "Chef d'équipe", users: [
-            Credit(nom: "Guillaume Rozier", image: "https://github.com/rozierguillaume.png", role: "Le grand chef"),
-        ]),
-        CreditSection(section: "Team iOS", users: [
-            Credit(nom: "Victor Sarda", image: "https://github.com/victor-sarda.png", role: "Développeur"),
-            Credit(nom: "Paul Jeannot", image: "https://github.com/pauljeannot.png", role: "Développeur"),
-            Credit(nom: "Nathan Fallet", image: "https://github.com/NathanFallet.png", role: "Dev & App Store"),
-            Credit(nom: "Michaël Nass", image: "https://github.com/PlugNCS.png", role: "App Store"),
-        ]),
-        CreditSection(section: "Team Android", users: [
-            Credit(nom: "Michel Gauzins", image: "https://github.com/michgauz.png", role: "Dev & Play Store"),
-            Credit(nom: "JB Dumoulin", image: "https://github.com/dumoulinjb.png", role: "Développeur"),
-        ]),
-    ]
-}
