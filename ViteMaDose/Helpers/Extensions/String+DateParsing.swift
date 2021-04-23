60d9967ec5cd82aa4cba1b599da36289f2c085c5
PlugN <michael.nass@free.fr>
2021-04-24 01:05:48 +0200

    Fix Merge Conflict

--- ViteMaDose/Helpers/Extensions/String+DateParsing.swift
+++ ViteMaDose/Helpers/Extensions/String+DateParsing.swift
@@ -19,4 +19,17 @@ extension String {
         let date = toDate(nil, region: region) ?? toISODate(nil, region: region)
         return date?.toString(style)
     }
+    
+    // Localization
+    func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
+        return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
+    }
+    
+    func format(_ args : CVarArg...) -> String {
+        return String(format: self, locale: .current, arguments: args)
+    }
+        
+    func format(_ args : [String]) -> String {
+        return String(format: self, locale: .current, arguments: args)
+    }
 }
