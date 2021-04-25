6fa9753ae93915bc1ced0baecc9cf53c8dfc7d40
Victor Sarda <victor.sarda@icloud.com>
2021-04-25 02:56:05 +0100

    🔧 Tests environment setup

--- ViteMaDoseTests/ViewModels/Home/HomeViewModelTests.swift
+++ ViteMaDoseTests/ViewModels/Home/HomeViewModelTests.swift
@@ -1,14 +1,13 @@
 //
-//  ViteMaDoseTests.swift
+//  HomeViewModelTests.swift
 //  ViteMaDoseTests
 //
-//  Created by Victor Sarda on 07/04/2021.
+//  Created by Victor Sarda on 24/04/2021.
 //
 
 import XCTest
-@testable import ViteMaDose
 
-class ViteMaDoseTests: XCTestCase {
+class HomeViewModelTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
