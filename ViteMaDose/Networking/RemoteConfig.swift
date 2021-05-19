b5c140321914931d42ecf053c0eb8c3a1bfbfaa1
CorentiOS <43886135+CorentiOS@users.noreply.github.com>
2021-05-19 23:11:03 +0200

    Update ViteMaDose/Networking/RemoteConfig.swift
    Co-authored-by: Victor Sarda <victor.sarda@icloud.com>

--- ViteMaDose/Networking/RemoteConfig.swift
+++ ViteMaDose/Networking/RemoteConfig.swift
@@ -77,8 +77,8 @@ extension RemoteConfiguration {
         return configuration.configValue(forKey: "vaccination_centres_list_radius_in_km").numberValue
     }
 
-    var chronodoseMinCount: NSNumber {
-        return configuration.configValue(forKey: "chronodose_min_count").numberValue
+    var chronodoseMinCount: Int {
+        return configuration.configValue(forKey: "chronodose_min_count").numberValue.intValue
     }
 
     var vaccinationCentresListRadiusInMeters: Double {
