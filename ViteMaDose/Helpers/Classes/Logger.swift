b5737e135a534546d5e39d5ea64fbda31cfe19e0
Pierre-Yves Lapersonne <dev@pylapersonne.info>
2022-01-01 19:10:30 +0100

    chore: #157 - update copyright
    Signed-off-by: Pierre-Yves Lapersonne <dev@pylapersonne.info>

--- ViteMaDose/Helpers/Classes/Logger.swift
+++ ViteMaDose/Helpers/Classes/Logger.swift
@@ -1,5 +1,5 @@
 // Software Name: vitemadose-ios
-// SPDX-FileCopyrightText: Copyright (c) 2021 CovidTracker
+// SPDX-FileCopyrightText: Copyright (c) 2021 CovidTracker.fr
 // SPDX-License-Identifier: GNU General Public License v3.0 or later
 //
 // This software is distributed under the GPL-3.0-or-later license.
